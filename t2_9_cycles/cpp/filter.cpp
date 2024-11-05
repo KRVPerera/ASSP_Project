71f192e209a93c858fc43a5a5287926a303d0ec6
KRVPerera <2173530+KRVPerera@users.noreply.github.com>
2024-11-05 22:40:37 +0200

    9 cycles

---
+++ t2_9_cycles/cpp/filter.cpp
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include "utils.h"
+#include <vector>
+
+int main() {
+    short status = 1;
+    int count = 0;
+    std::streampos* lastSeekPoint = NULL;
+    lastSeekPoint = new std::streampos;
+    *lastSeekPoint = 0;
+    short data[8];
+
+    int global_line_count = 0;
+    std::vector<short> datas = readFileLineByLine("input.in");
+
+    printf("status\n");
+    while (status > 0) {
+        short data[8];
+
+
+        int j = 0;
+        for (size_t i = global_line_count; i < 8; i++)
+        {
+            data[j] = datas[i];
+            j++;
+            global_line_count = i;
+        }
+
+
+        std::vector<int> data_out(8);
+        for (int i = 0; i < 8; i++) {
+
+            printf("data[%d] = %d\n", i, data[i]);
+
+            data_out.push_back(data[i]);
+        }
+
+        appendIntValuesToFile("OUR_OUT.out", data_out);
+
+        count += 1;
+        if (count > 2) {
+            status = 0;
+        }
+    }
+
+    return 0;
+}
+
