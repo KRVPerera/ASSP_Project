438763e4228942afa8fc3ecb50481fce2401c5ea
KRVPerera <2173530+KRVPerera@users.noreply.github.com>
2024-11-07 22:41:52 +0200

    fix: rid of int type from fir.c

--- t2_opt/fir.c
+++ t2_opt/fir.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdint.h>
 
 #define SCALE 8
 #define K0 37
@@ -6,44 +7,46 @@
 #define K2 109
 #define K3 37
 
-int mul(int a, int b) {
+inline int16_t mul(int8_t a, int8_t b) {
     return (a * b);
 }
 
-int fir_filter(int x0, int x1, int x2, int x3) {
+inline int8_t fir_filter(int8_t x0, int8_t x1, int8_t x2, int8_t x3) {
     return (mul(K0, x0) + mul(K1, x1) + mul(K2, x2) + mul(K3, x3)) >> SCALE;
 }
 
-void __attribute__((noinline)) fir_filter_loop(int *restrict x) {
-    int data, status, done = 0;
+void __attribute__((noinline)) fir_filter_loop(int8_t *restrict x) {
+    int status, done = 0;
+    uint8_t data;
 
-    _TCE_FIFO_S16_STREAM_IN(0, data, status);
+    _TCE_FIFO_U8_STREAM_IN(0, data, status);
     do {
         x[3] = x[2];
         x[2] = x[1];
         x[1] = x[0];
-        x[0] = data - 128; 
+        x[0] = (int8_t)data - 128; 
 
-        int result = fir_filter(x[0], x[1], x[2], x[3]);
-        _TCE_FIFO_S16_STREAM_OUT(result + 128);  
+        int8_t result = fir_filter(x[0], x[1], x[2], x[3]);
+        _TCE_FIFO_U8_STREAM_OUT(result + 128);  
 
         done = (status == 0);
-        _TCE_FIFO_S16_STREAM_IN(0, data, status);
-
+        _TCE_FIFO_U8_STREAM_IN(0, data, status);
+        //printf("Line 1\n");
     } while (!done);
 }
 
 void sample_copy(int count) {
-    int i, temp, status;
+    int i, status = 0;
+    uint8_t temp;
     for (i = 0; i < count; i++) {
-        _TCE_FIFO_S16_STREAM_IN(0, temp, status);
-        _TCE_FIFO_S16_STREAM_OUT(temp);
+        _TCE_FIFO_U8_STREAM_IN(0, temp, status);
+        _TCE_FIFO_U8_STREAM_OUT(temp);
     }
 }
 
 int main(int argc, char *argv[]) {
     sample_copy(44);
-    int x[4] = {0, 0, 0, 0};
+    int8_t x[4] = {0, 0, 0, 0};
     fir_filter_loop(x);
     return 0;
 }
