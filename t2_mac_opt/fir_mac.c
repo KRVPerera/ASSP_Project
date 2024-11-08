0d6df07a35f0b7f43952dc0762b0553bb022b618
KRVPerera <2173530+KRVPerera@users.noreply.github.com>
2024-11-08 02:09:58 +0200

    fix: use unsigned when reading

---
+++ t2_mac_opt/fir_mac.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdint.h>
+
+#define SCALE 8
+#define K0 37
+#define K1 109
+#define K2 109
+#define K3 37
+
+void __attribute__((noinline)) fir_filter_loop(int8_t *restrict x) {
+    int status, done = 0;
+    uint8_t data;
+    _TCE_FIFO_U8_STREAM_IN(0, data, status);
+    do {
+        int8_t result;
+        int16_t res1;
+        int16_t res2;
+        int16_t res3;
+        int16_t temp_result;
+        x[3] = x[2];
+        x[2] = x[1];
+        x[1] = x[0];
+        x[0] = data - 128; 
+
+        _TCE_DILANMAC(K0, x[0], 0, res1);
+        _TCE_DILANMAC(K1, x[1], res1, res2);
+        _TCE_DILANMAC(K2, x[2], res2, res3);
+        _TCE_DILANMAC(K3, x[3], res3, temp_result);
+
+        result = temp_result >> SCALE;
+
+        _TCE_FIFO_U8_STREAM_OUT(result + 128);  
+
+        done = (status == 0);
+        _TCE_FIFO_U8_STREAM_IN(0, data, status);
+        //printf("Line 1\n");
+
+    } while (!done);
+}
+
+void sample_copy(int count) {
+    int i, status = 0;
+    uint8_t temp;
+    for (i = 0; i < count; i++) {
+        _TCE_FIFO_U8_STREAM_IN(0, temp, status);
+        _TCE_FIFO_U8_STREAM_OUT(temp);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    sample_copy(44);
+    int8_t x[4] = {0, 0, 0, 0};
+    fir_filter_loop(x);
+    return 0;
+}
