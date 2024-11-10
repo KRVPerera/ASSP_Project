922bba2c346d437833b912f75c0c8d683e114bd8
KRVPerera <2173530+KRVPerera@users.noreply.github.com>
2024-11-10 14:40:25 +0200

    farrow okay

--- t4_farrow/farrow.c
+++ t4_farrow/farrow.c
@@ -1,24 +1,27 @@
 #include <stdio.h>
 #include <stdint.h>
 
-#define SCALE 1024
+#define SCALE 16
 
-void __attribute__((noinline)) filter_loop(int8_t *restrict x) {
+void __attribute__((noinline)) filter_loop(int *restrict x) {
     int status1, status2, done = 0;
     uint8_t signal1;
     uint8_t d_sig;
+
     do {
-        _TCEFU_FIFO_U8_STREAM_IN("INPUT_2", 0, signal1, status1);
         _TCEFU_FIFO_U8_STREAM_IN("INPUT_1", 0, d_sig, status2);
+        _TCEFU_FIFO_U8_STREAM_IN("INPUT_2", 0, signal1, status1);
         done = (status1 == 0);
-        int16_t result;
         x[1] = x[0];
         x[0] = signal1;
-        int16_t delay = ((int16_t)d_sig) << 10 >> 4;
+        int8_t delay = d_sig;
+        int8_t diff = 16 - delay;
+        int mul1 = x[0] * diff;
+        int mul2 = x[1] * delay;
+        int result = (mul1 + mul2) >> 4;
+        uint8_t uresult = (uint8_t)result;
 
-        result = ((x[0] * (1024 - delay)) + (delay * x[1])) >> 10;
-
-        _TCE_FIFO_U8_STREAM_OUT(result);
+        _TCE_FIFO_U8_STREAM_OUT(uresult);
     } while (!done);
 }
 
@@ -34,7 +37,7 @@ void sample_copy(int count) {
 
 int main(int argc, char *argv[]) {
     sample_copy(44);
-    int8_t x[2] = {0, 0};
+    int x[2] = {0, 0};
     filter_loop(x);
     return 0;
 }
