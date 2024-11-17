cfa17ff0dca7e0685de04a408017dbc30a20f43d
KRVPerera <2173530+KRVPerera@users.noreply.github.com>
2024-11-17 23:26:45 +0200

    optimize processor vhdl

--- t2_opt/create_vhdl.sh
+++ t2_opt/create_vhdl.sh
@@ -2,3 +2,5 @@
 
 createbem processor.adf
 generateprocessor -b processor.bem -e toplevel -l 'vhdl' -i processor.idf -o hdl_files processor.adf
+generatebits -e toplevel -b processor.bem -d -w 4 -p fir.tpef -x hdl_files -f 'mif' -o 'mif' processor.adf
+
